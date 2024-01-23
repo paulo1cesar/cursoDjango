eba608169f2361138269daf77d941f0c8fdbc800
Paulo César <paulo1truester@gmail.com>
2024-01-23 17:43:32 -0300

    atualizacao

--- djangoProjeto/settings.py
+++ djangoProjeto/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'djangoProjeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_template',
+            BASE_DIR / 'recipes' / 'templates'   
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'base_static/'
+STATIC_ROOT = BASE_DIR/ 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    serving = models.IntegerField()
+    serving_unit = models.CharField(max_length=65)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d')
