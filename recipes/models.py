8d6ddb610a48152bfbb18d514af75385dbfef9b1
Paulo César <paulo1truester@gmail.com>
2024-01-30 23:42:51 -0300

    criacao Models

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class category(models.Model):
+    name = models.CharField(max_length=20)
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -14,3 +18,5 @@ class Recipe(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d')
+    categoria = models.ForeignKey(category, on_delete=models.SET_NULL, null=True)
+    categoria = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
